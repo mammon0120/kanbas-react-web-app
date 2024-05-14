d50a656faa35f31c5a8d5ac14728a5513be3278c
mammon0120 <mammon0120111@gmail.com>
2024-05-13 19:35:34 -0700

    add my name and section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ import TOC from "./TOC";
 export default function Labs() {
   return (
     <div>
+      <h1>Yumei Wang Summer1 2024</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
@@ -15,6 +16,7 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
+      <a href="https://github.com/mammon0120/kanbas-react-web-app.git">GitHub</a>
 
     </div>
   );
