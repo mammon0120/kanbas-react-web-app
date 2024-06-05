830559b8bd1c8cac5d6bc088bf2d39f9d18fe44a
mammon0120 <mammon0120111@gmail.com>
2024-06-05 10:55:14 -0700

    worked on styling kanbas

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -16,8 +16,7 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
-      <h4>My GitHub repository</h4>
-            <a id="wd-github" href="https://github.com/mammon0120/kanbas-react-web-app.git"> GitHub</a>
+      
     </div>
   );
 }  
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
