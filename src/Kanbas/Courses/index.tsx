111e1efc418bfdce59b2a34c4c7a05450372ee34
mammon0120 <mammon0120111@gmail.com>
2024-07-04 19:26:50 -0700

    a3 javascript

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,18 +1,24 @@
 import CoursesNavigation from "./Navigation";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Grades from "./Grades";
 import { RxTextAlignJustify } from "react-icons/rx";
+import { courses } from "../Database";
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
+
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <RxTextAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234</h2>
+        {course && course.name} &gt; {pathname.split("/")[4]}
+        </h2>
       <hr />
 
       <div className="d-flex">
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -15,9 +15,12 @@ export default function Kanbas() {
 
         <div className="flex-fill p-4">
           <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
             <Route path="Dashboard" element={<Dashboard />} />
-            <Route path="Courses/:id/*" element={<Courses />} />
+            <Route path="Courses/:cid/*" element={<Courses />} />
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+            <Route path="Inbox" element={<h1>Inbox</h1>} />
           </Routes>
         </div>
         
